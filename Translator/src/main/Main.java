3182336c0d5148eca98c228fb55bf803ab2da5cc
Donkid33 <35999999+Donkid33@users.noreply.github.com>
2020-04-11 21:44:54 -0400

    Created an empty class creator that creates empty classes
    title

--- Translator/src/main/Main.java
+++ Translator/src/main/Main.java
@@ -1,10 +1,21 @@
 package main;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		
-		System.out.println("this would make an empty java file ig");
+		
+		PrintWriter writer = new PrintWriter("emptyClass.java", "UTF-8");
+		writer.println("public class emptyClass{");
+		writer.println("public static void main (String[] args){");
+		writer.println("System.out.println(\"Look at me, I'm Incepting!\");");
+		writer.println("}");
+		writer.println("}");
+		writer.close();
 	}
 	
 	//TODO: EVERYTHING
