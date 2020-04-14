8ef6e833888655f68dd74a3a12281192f9d9ff2c
Donkid33 <35999999+Donkid33@users.noreply.github.com>
2020-04-14 18:35:35 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/CPS240-Final-Team/GenerateTheSpire""
    This reverts commit caf718a2650fcbaed07fd43d13ace73ebef17a24.

---
+++ ParamterClass/src/CardParameters.java
@@ -0,0 +1,20 @@
+import Enums.CardColor;
+import Enums.CardRarity;
+import Enums.CardSpecialEffects;
+import Enums.CardType;
+
+public class CardParameters 
+{
+
+	public static String ID;
+	public static String cardName;
+	public static String cardDescription;
+	public static int cardCost;
+	public static CardType cardType;
+	public static CardSpecialEffects[] cardSpecialEffects;
+	//triggers
+	public static CardColor cardColor;
+	public static CardRarity cardRarity;
+	
+	
+}
